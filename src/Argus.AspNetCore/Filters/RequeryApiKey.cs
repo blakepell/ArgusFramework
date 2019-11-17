a9793ede851704fb304c6d1c92c4c7068c1fe67b
blakepell <blakepell@hotmail.com>
2019-11-17 17:41:57 -0500

    Two additional classes.

---
+++ src/Argus.AspNetCore/Filters/RequeryApiKey.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Argus.AspNetCore.Filters
+{
+    /// <summary>
+    ///     Attribute that will check that an API key has been provided.
+    /// </summary>
+    public class RequireApiKey : Attribute, IAuthorizationFilter
+    {
+        private readonly string _apiKeyName = "";
+        private readonly string _queryField = "";
+        private readonly string _unauthorizedMessage = "Error - Unauthorized";
+
+        /// <summary>
+        ///     Constructor (compares against the authorization header/querystring/form field value).  This requires that the Configuration
+        ///     has been setup in the Startup and that the service has been registered via:
+        ///     <code>services.AddSingleton&lt;IConfigurationRoot&gt;(Configuration);</code>
+        /// </summary>
+        /// <param name="apiKeyName">The name of the setting in the config that holds the API key to validate.</param>
+        public RequireApiKey(string apiKeyName)
+        {
+            _apiKeyName = apiKeyName;
+            _queryField = "authorization";
+        }
+
+        /// <summary>
+        ///     Constructor (compares against the authorization header/querystring/form field value).  This requires that the Configuration
+        ///     has been setup in the Startup and that the service has been registered via:
+        ///     <code>services.AddSingleton&lt;IConfigurationRoot&gt;(Configuration);</code>
+        /// </summary>
+        /// <param name="apiKeyName">The name of the setting in the config that holds the API key to validate.</param>
+        /// <param name="queryField">The name of the header/querystring/form element to compare the key value to.</param>
+        public RequireApiKey(string apiKeyName, string queryField)
+        {
+            _apiKeyName = apiKeyName;
+            _queryField = queryField;
+        }
+
+        /// <summary>
+        ///     Constructor (compares against the authorization header/querystring/form field value).  This requires that the Configuration
+        ///     has been setup in the Startup and that the service has been registered via:
+        ///     <code>services.AddSingleton&lt;IConfigurationRoot&gt;(Configuration);</code>
+        /// </summary>
+        /// <param name="apiKeyName">The name of the setting in the config that holds the API key to validate.</param>
+        /// <param name="queryField">The name of the header/querystring/form element to compare the key value to.</param>
+        /// <param name="unauthorizedMessage">The message to return with the 401 unauthorized result if the result is deemed invalid.</param>
+        public RequireApiKey(string apiKeyName, string queryField, string unauthorizedMessage)
+        {
+            _apiKeyName = apiKeyName;
+            _queryField = queryField;
+            _unauthorizedMessage = unauthorizedMessage;
+        }
+
+        /// <summary>
+        ///     API Key Validation Event
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool result = false;
+
+            var config = context.HttpContext.RequestServices.GetRequiredService<IConfigurationRoot>();
+
+            string configApiKey = config[$"AppSettings:{_apiKeyName}"];
+
+            if (context.HttpContext.Request.Headers.Keys.Contains(_queryField)
+                && context.HttpContext.Request.Headers[_queryField] == configApiKey)
+            {
+                result = true;
+            }
+
+            if (context.HttpContext.Request.HasFormContentType
+                && context.HttpContext.Request.Form.Keys.Contains(_queryField)
+                && context.HttpContext.Request.Form[_queryField] == configApiKey)
+            {
+                result = true;
+            }
+
+            if (context.HttpContext.Request.Query.Keys.Contains(_queryField)
+                && context.HttpContext.Request.Query[_queryField] == configApiKey)
+            {
+                result = true;
+            }
+
+            // Failure
+            if (!result)
+            {
+                // Toss a 401 unauthorized and specify the error message
+                context.Result = new ObjectResult(_unauthorizedMessage);
+                context.HttpContext.Response.StatusCode = 401;
+            }
+        }
+    }
+}
---
+++ src/Argus.AspNetCore/TagHelpers/GravatarTagHelper.cs
@@ -0,0 +1,83 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Argus.AspNetCore.TagHelpers
+{
+    /// <summary>
+    ///     <img gravatar-email="@User.GetEmail()" gravatar-size="30" />
+    /// </summary>
+    [HtmlTargetElement("img", Attributes = EmailAttributeName)]
+    public class GravatarTagHelper : TagHelper
+    {
+        private const string EmailAttributeName = "gravatar-email";
+        private const string SizeAttributeName = "gravatar-size";
+        private const string DefaultAttributeName = "gravatar-default-image";
+        private const string RatingAttributeName = "gravatar-rating";
+        private const string httpEndpointFormat = "http://gravatar.com/avatar/{0}?s={1}&d={2}&r={3}";
+        private const string httpsEndpointFormat = "https://secure.gravatar.com/avatar/{0}?s={1}&d={2}&r={3}";
+
+        public GravatarTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+
+        protected internal IHttpContextAccessor contextAccessor { get; set; }
+
+        [HtmlAttributeName(EmailAttributeName)]
+        public string EmailAddress { get; set; }
+
+        [HtmlAttributeName(SizeAttributeName)] public int Size { get; set; } = 80;
+
+        [HtmlAttributeName(DefaultAttributeName)]
+        public string DefaultImage { get; set; } = "mm";
+
+        [HtmlAttributeName(RatingAttributeName)]
+        public string Rating { get; set; } = "g";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string emailHash;
+
+            using (var md5 = MD5.Create())
+            {
+                var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(this.EmailAddress));
+
+
+                var sb = new StringBuilder();
+
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("x2"));
+                }
+
+                emailHash = sb.ToString();
+            }
+
+            string scheme = this.contextAccessor.HttpContext.Request.Scheme;
+            string format;
+
+            if (scheme == "https")
+            {
+                format = httpsEndpointFormat;
+            }
+            else
+            {
+                format = httpEndpointFormat;
+            }
+
+            string url = string.Format(
+                CultureInfo.InvariantCulture,
+                format,
+                emailHash, this.Size, this.DefaultImage, this.Rating
+            );
+
+            var att = new TagHelperAttribute("gravatar-email");
+            output.Attributes.Remove(att);
+
+            output.Attributes.Add("src", url);
+        }
+    }
+}
