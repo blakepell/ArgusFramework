a345c79bb52f31eeec1fc7bb06935060e6933585
Blake Pell <blakepell@hotmail.com>
2019-11-18 11:35:25 -0500

    WinForm and WPF test projects.

---
+++ tests/WinFormsTestApp/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinFormsTestApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ButtonDoSomething = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(559, 230);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // ButtonDoSomething
+            // 
+            this.ButtonDoSomething.Location = new System.Drawing.Point(459, 248);
+            this.ButtonDoSomething.Name = "ButtonDoSomething";
+            this.ButtonDoSomething.Size = new System.Drawing.Size(112, 34);
+            this.ButtonDoSomething.TabIndex = 1;
+            this.ButtonDoSomething.Text = "Do Something";
+            this.ButtonDoSomething.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 293);
+            this.Controls.Add(this.ButtonDoSomething);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Main";
+            this.Text = "Test Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button ButtonDoSomething;
+    }
+}
+
