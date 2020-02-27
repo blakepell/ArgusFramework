c659aedc7280142918c50f96d0c245246691ddcd
blakepell <blakepell@hotmail.com>
2020-02-27 18:24:43 -0500

    Benchmark project.

---
+++ tests/BenchmarkConsoleProject/BenchmarkConsoleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace BenchmarkConsoleProject
+{
+    class Program
+    {
+        [Benchmark]
+        public void Slow() => Thread.Sleep(100);
+
+        [Benchmark]
+        public void Fast() => Thread.Sleep(25);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BenchmarkDotNet - Hello World!");
+            var summary = BenchmarkRunner.Run<Program>();
+        }
+    }
+}
