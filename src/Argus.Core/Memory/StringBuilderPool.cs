45b995088212b06675ea9ed4a74faf2c3387e659
Blake Pell <bpell@iu.edu>
2020-02-27 17:06:32 -0500

    StringBuilderPool.

---
+++ src/Argus.Core/Memory/StringBuilderPool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace Argus.Memory
+{
+    /// <summary>
+    /// A thread safe static pool of StringBuilders.
+    /// </summary>
+    public static class StringBuilderPool
+    {
+        private const int MaxPooledStringBuilders = 64;
+        
+        private static readonly ConcurrentQueue<StringBuilder> Pool = new ConcurrentQueue<StringBuilder>();
+
+        /// <summary>
+        /// Returns a StringBuilder from the pool.  If no StringBuilder is available a new one will
+        /// be allocated and returned.
+        /// </summary>
+        /// <returns></returns>
+        public static StringBuilder Take()
+        {
+            if (Pool.TryDequeue(out var sb))
+            {
+                return sb;
+            }
+
+            return new StringBuilder();
+        }
+
+        /// <summary>
+        /// Returns a StringBuilder to the pool.
+        /// </summary>
+        /// <param name="sb"></param>
+        public static void Return(StringBuilder sb)
+        {
+            if (Pool.Count <= MaxPooledStringBuilders)
+            {
+                // There is a race condition here so the count could be off a little bit (but insignificantly)
+                sb.Clear();
+                Pool.Enqueue(sb);
+            }
+        }
+    }
+}
