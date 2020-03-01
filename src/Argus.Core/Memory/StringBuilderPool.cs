f2cf64824e06d927811db0d2cd04d14d695c217f
blakepell <blakepell@hotmail.com>
2020-02-29 22:56:59 -0500

    StringBuilderPool Take overloads for populating with a string, StringBuilder and ReadOnlySpan<char>

--- src/Argus.Core/Memory/StringBuilderPool.cs
+++ src/Argus.Core/Memory/StringBuilderPool.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Text;
 
 namespace Argus.Memory
@@ -16,19 +17,72 @@ namespace Argus.Memory
         /// Returns a StringBuilder from the pool.  If no StringBuilder is available a new one will
         /// be allocated and returned.
         /// </summary>
-        /// <returns></returns>
         public static StringBuilder Take()
         {
             if (Pool.TryDequeue(out var sb))
             {
                 return sb;
             }
-
+            
             return new StringBuilder();
         }
 
         /// <summary>
-        /// Returns a StringBuilder to the pool.
+        /// Returns a StringBuilder from the pool and populates it with the specified value.  If no
+        /// StringBuilder is available a new one will be allocated and returned.
+        /// </summary>
+        /// <param name="buf"></param>
+        public static StringBuilder Take(string buf)
+        {
+            if (Pool.TryDequeue(out var sb))
+            {
+                sb.Append(buf);
+                return sb;
+            }
+
+            return new StringBuilder(buf);
+        }
+
+        /// <summary>
+        /// Returns a StringBuilder from the pool and populates it with the specified value.  If no
+        /// StringBuilder is available a new one will be allocated and returned.
+        /// </summary>
+        /// <param name="sbCopy"></param>
+        public static StringBuilder Take(StringBuilder sbCopy)
+        {
+            if (Pool.TryDequeue(out var sb))
+            {
+                sb.Append(sbCopy);
+                return sb;
+            }
+
+            var sbSpan = new StringBuilder();
+            sbSpan.Append(sbCopy);
+            return sbSpan;
+        }
+
+#if NETSTANDARD2_1
+        /// <summary>
+        /// Returns a StringBuilder from the pool and populates it with the specified ReadOnlySpan.  If no
+        /// StringBuilder is available a new one will be allocated and returned.        
+        /// </summary>
+        /// <param name="span"></param>
+        public static StringBuilder Take(ReadOnlySpan<char> span)
+        {
+            if (Pool.TryDequeue(out var sb))
+            {
+                sb.Append(span);
+                return sb;
+            }
+
+            var sbSpan = new StringBuilder();
+            sbSpan.Append(span);
+            return sbSpan;
+        }
+#endif
+
+        /// <summary>
+        /// Returns a StringBuilder to the pool and clears its contents.
         /// </summary>
         /// <param name="sb"></param>
         public static void Return(StringBuilder sb)
