cc0fc104f87cee1054e7a4c67bdfcdb4618bedbb
blakepell <blakepell@hotmail.com>
2020-09-28 12:08:05 -0400

    Bool/Nullable Bool extension methods.

---
+++ src/Argus.Core/Extensions/BoolExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace Argus.Extensions
+{
+    /// <summary>
+    ///     bool and nullable bool extension methods.
+    /// </summary>
+    public static class BoolExtensions
+    {
+        //*********************************************************************************************************************
+        //
+        //            Module:  BoolExtensions
+        //      Organization:  http://www.blakepell.com
+        //      Initial Date:  09/28/2020
+        //      Last Updated:  09/28/2020
+        //     Programmer(s):  Blake Pell, blakepell@hotmail.com
+        //
+        //*********************************************************************************************************************
+
+        /// <summary>
+        /// Returns a True only if the value is true.  Null is considered false.
+        /// </summary>
+        /// <param name="value"></param>
+        public static bool IsTrue(this bool? value)
+        {
+            if (value == null || !value.Value)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns a False if the value is False or null.
+        /// </summary>
+        /// <param name="value"></param>
+        public static bool IsFalse(this bool? value)
+        {
+            if (value == null || !value.Value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
