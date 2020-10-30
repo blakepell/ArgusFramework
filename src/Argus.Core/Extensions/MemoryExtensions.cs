e68e66e4bb62359691c872e71511c8697392f237
blakepell <blakepell@hotmail.com>
2020-10-29 21:35:29 -0400

    SafeIndexOf extension added for ReadOnlySpan.  IndexOf extension provided a char fixed with 'this'.

--- src/Argus.Core/Extensions/MemoryExtensions.cs
+++ src/Argus.Core/Extensions/MemoryExtensions.cs
@@ -13,7 +13,7 @@ namespace Argus.Extensions
         //             Class:  MemoryExtensions
         //      Organization:  http://www.blakepell.com
         //      Initial Date:  02/12/2020
-        //      Last Updated:  03/01/2020
+        //      Last Updated:  10/29/2020
         //
         //*********************************************************************************************************************
 
@@ -28,6 +28,26 @@ namespace Argus.Extensions
         ///     startIndex greater than the length of the string.
         /// </returns>
         public static int SafeIndexOf(this ReadOnlySpan<char> span, ReadOnlySpan<char> value, int startIndex)
+        {
+            if (startIndex > span.Length - 1)
+            {
+                return -1;
+            }
+
+            return span.IndexOf(value, startIndex);
+        }
+
+        /// <summary>
+        ///     Reports the zero based index of the first occurence of a matching char.
+        /// </summary>
+        /// <param name="span">The Span to search.</param>
+        /// <param name="value">The string to search for.</param>
+        /// <param name="startIndex"></param>
+        /// <returns>
+        ///     Returns the zero based index or a -1 if the char isn't found or the
+        ///     startIndex greater than the length of the string.
+        /// </returns>
+        public static int SafeIndexOf(this ReadOnlySpan<char> span, char value, int startIndex)
         {
             if (startIndex > span.Length - 1)
             {
@@ -61,7 +81,7 @@ namespace Argus.Extensions
         /// <param name="span"></param>
         /// <param name="value"></param>
         /// <param name="startIndex"></param>
-        public static int IndexOf(ReadOnlySpan<char> span, char value, int startIndex)
+        public static int IndexOf(this ReadOnlySpan<char> span, char value, int startIndex)
         {
             var indexInSlice = span.Slice(startIndex).IndexOf(value);
 
