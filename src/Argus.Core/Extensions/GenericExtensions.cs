2698a1232c371abe9107aab3dce043a2251eed18
Blake Pell <blakepell@hotmail.com>
2021-02-04 11:35:12 -0500

    ArrayExtensions merged into GenericExtensions.

--- src/Argus.Core/Extensions/GenericExtensions.cs
+++ src/Argus.Core/Extensions/GenericExtensions.cs
@@ -177,6 +177,29 @@ namespace Argus.Extensions
         }
 
         /// <summary>
+        /// Remove element from array at given index
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="index"></param>
+        public static T[] RemoveAt<T>(this T[] source, int index)
+        {
+            var destination = new T[source.Length - 1];
+
+            if (index > 0)
+            {
+                Array.Copy(source, 0, destination, 0, index);
+            }
+
+            if (index < source.Length - 1)
+            {
+                Array.Copy(source, index + 1, destination, index, source.Length - index - 1);
+            }
+
+            return destination;
+        }
+
+        /// <summary>
         /// Turns an IEnumerable into a Markdown table.
         /// </summary>
         /// <typeparam name="T"></typeparam>
