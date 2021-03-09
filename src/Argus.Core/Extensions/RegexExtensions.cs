66b064e8136463125e6be6b885bf33371f218911
Blake Pell <blakepell@hotmail.com>
2021-03-09 17:01:35 -0500

    Regular expressions TrySuccess extension method.

---
+++ src/Argus.Core/Extensions/RegexExtensions.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * @author            : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @initial date      : 2021-03-09
+ * @last updated      : 2021-03-09
+ * @copyright         : Copyright (c) 2003-2021, All rights reserved.
+ * @license           : MIT
+ */
+
+using System.Text.RegularExpressions;
+
+namespace Argus.Extensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="System.Text.RegularExpressions" /> namespace.
+    /// </summary>
+    public static class RegexExtensions
+    {
+        /// <summary>
+        /// Returns a true or false with a null value returned as a false.
+        /// </summary>
+        /// <param name="match"></param>
+        public static bool TrySuccess(this Match match)
+        {
+            return match?.Success ?? false;
+        }
+    }
+}
