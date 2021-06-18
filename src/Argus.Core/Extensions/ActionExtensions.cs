22b2e9c85b9f0deb0c6d873cfeba77a7ea554bc2
Blake Pell <blakepell@hotmail.com>
2021-06-18 18:24:58 -0400

    Class for Action extension methods.

---
+++ src/Argus.Core/Extensions/ActionExtensions.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * @author            : Blake Pell
+ * @website           : http://www.blakepell.com
+ * @initial date      : 2021-05-06
+ * @last updated      : 2021-05-06
+ * @copyright         : Copyright (c) 2003-2021, All rights reserved.
+ * @license           : MIT
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Argus.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="Action"/>
+    /// </summary>
+    public static class ActionExtensions
+    {
+        /// <summary>
+        /// Provides the ability to await an <see cref="Action"/>.
+        /// </summary>
+        /// <param name="action"></param>
+        public static TaskAwaiter GetAwaiter(this Action action)
+        {
+            var task = new Task(action);
+            task.Start();
+
+            return task.GetAwaiter();
+        }
+    }
+}
