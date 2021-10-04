ab1c9684fe8df924d7f1b5fa7e82a70d7a381498
Blake Pell <blakepell@hotmail.com>
2021-10-04 10:48:27 -0400

    Enum/bit bug fixes and perf changes.

--- src/Argus.Core/Extensions/EnumExtensions.cs
+++ src/Argus.Core/Extensions/EnumExtensions.cs
@@ -2,7 +2,7 @@
  * @author            : Blake Pell
  * @website           : http://www.blakepell.com
  * @initial date      : 2016-03-01
- * @last updated      : 2021-10-03
+ * @last updated      : 2021-10-04
  * @copyright         : Copyright (c) 2003-2021, All rights reserved.
  * @license           : MIT
  */
@@ -24,25 +24,24 @@ namespace Argus.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="enumValue">A flags enum.</param>
         /// <param name="bit">A single bit or multiple bits. Use | to check or for multiple bits and &amp; to check for all bits.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsBitSet<T>(this T enumValue, T bit) where T : Enum
         {
-            if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<ulong>())
+            var size = Unsafe.SizeOf<T>();
+
+            if (size == 8)
             {
                 return (enumValue.AsULong() & bit.AsULong()) != 0;
             }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<long>())
-            {
-                return (enumValue.AsLong() & bit.AsLong()) != 0;
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<int>())
-            {
-                return (enumValue.AsInt() & bit.AsInt()) != 0;
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<uint>())
+            else if (size == 4)
             {
                 return (enumValue.AsUInt() & bit.AsUInt()) != 0;
             }
-            
+            else if (size == 2)
+            {
+                return (enumValue.AsUShort() & bit.AsUShort()) != 0;
+            }
+
             throw new Exception("Type mismatch.");
         }
 
@@ -52,28 +51,26 @@ namespace Argus.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="enumValue"></param>
         /// <param name="bit"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T SetBit<T>(this T enumValue, T bit) where T : Enum
         {
-            if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<ulong>())
+            var size = Unsafe.SizeOf<T>();
+
+            if (size == 8)
             {
                 var result = (enumValue.AsULong() | bit.AsULong());
                 return Unsafe.As<ulong, T>(ref result);
             }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<long>())
-            {
-                var result = (enumValue.AsLong() | bit.AsLong());
-                return Unsafe.As<long, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<int>())
-            {
-                var result = (enumValue.AsInt() | bit.AsInt());
-                return Unsafe.As<int, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<uint>())
+            else if (size == 4)
             {
                 var result = (enumValue.AsUInt() | bit.AsUInt());
                 return Unsafe.As<uint, T>(ref result);
             }
+            else if (size == 2)
+            {
+                var result = (ushort)(enumValue.AsUShort() | bit.AsUShort());
+                return Unsafe.As<ushort, T>(ref result);
+            }
 
             throw new Exception("Type mismatch.");
         }
@@ -84,27 +81,25 @@ namespace Argus.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="enumValue"></param>
         /// <param name="bit"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T RemoveBit<T>(this T enumValue, T bit) where T : Enum
         {
-            if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<ulong>())
+            var size = Unsafe.SizeOf<T>();
+
+            if (size == 8)
             {
                 var result = (enumValue.AsULong() & ~bit.AsULong());
                 return Unsafe.As<ulong, T>(ref result);
             }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<long>())
-            {
-                var result = (enumValue.AsLong() & ~bit.AsLong());
-                return Unsafe.As<long, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<int>())
-            {
-                var result = (enumValue.AsInt() & ~bit.AsInt());
-                return Unsafe.As<int, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<uint>())
+            else if (size == 4)
             {
                 var result = (enumValue.AsUInt() & ~bit.AsUInt());
                 return Unsafe.As<uint, T>(ref result);
+            }
+            else if (size == 2)
+            {
+                var result = (ushort)(enumValue.AsUShort() & ~bit.AsUShort());
+                return Unsafe.As<ushort, T>(ref result);
             }
 
             throw new Exception("Type mismatch.");
@@ -116,27 +111,25 @@ namespace Argus.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="enumValue"></param>
         /// <param name="bit"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T ToggleBit<T>(this T enumValue, T bit) where T : Enum
         {
-            if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<ulong>())
+            var size = Unsafe.SizeOf<T>();
+
+            if (size == 8)
             {
                 var result = (enumValue.AsULong() ^ bit.AsULong());
                 return Unsafe.As<ulong, T>(ref result);
             }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<long>())
-            {
-                var result = (enumValue.AsLong() ^ bit.AsLong());
-                return Unsafe.As<long, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<int>())
-            {
-                var result = (enumValue.AsInt() ^ bit.AsInt());
-                return Unsafe.As<int, T>(ref result);
-            }
-            else if (Unsafe.SizeOf<T>() == Unsafe.SizeOf<uint>())
+            else if (size == 4)
             {
                 var result = (enumValue.AsUInt() ^ bit.AsUInt());
                 return Unsafe.As<uint, T>(ref result);
+            }
+            else if (size == 2)
+            {
+                var result = (ushort)(enumValue.AsUShort() ^ bit.AsUShort());
+                return Unsafe.As<ushort, T>(ref result);
             }
 
             throw new Exception("Type mismatch.");
@@ -149,8 +142,10 @@ namespace Argus.Extensions
         /// <typeparam name="TEnum"></typeparam>
         /// <typeparam name="TOutputType"></typeparam>
         /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TOutputType AsGeneric<TEnum, TOutputType>(this TEnum enumValue) where TEnum : Enum
         {
+            // We don't know the size of either so we have to get both.
             if (Unsafe.SizeOf<TEnum>() == Unsafe.SizeOf<TOutputType>())
             {
                 return Unsafe.As<TEnum, TOutputType>(ref enumValue);
@@ -165,9 +160,11 @@ namespace Argus.Extensions
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static long AsLong<TEnum>(this TEnum enumValue) where TEnum : Enum
         {
-            if (Unsafe.SizeOf<TEnum>() != Unsafe.SizeOf<long>())
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 8)
             {
                 throw new Exception("Type mismatch.");
             }
@@ -181,9 +178,11 @@ namespace Argus.Extensions
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong AsULong<TEnum>(this TEnum enumValue) where TEnum : Enum
         {
-            if (Unsafe.SizeOf<TEnum>() != Unsafe.SizeOf<ulong>())
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 8)
             {
                 throw new Exception("Type mismatch.");
             }
@@ -197,9 +196,11 @@ namespace Argus.Extensions
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int AsInt<TEnum>(this TEnum enumValue) where TEnum : Enum
         {
-            if (Unsafe.SizeOf<TEnum>() != Unsafe.SizeOf<int>())
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 4)
             {
                 throw new Exception("Type mismatch.");
             }
@@ -213,14 +214,52 @@ namespace Argus.Extensions
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint AsUInt<TEnum>(this TEnum enumValue) where TEnum : Enum
         {
-            if (Unsafe.SizeOf<TEnum>() != Unsafe.SizeOf<uint>())
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 4)
             {
                 throw new Exception("Type mismatch.");
             }
 
             return Unsafe.As<TEnum, uint>(ref enumValue);
+        }
+
+        /// <summary>
+        /// Casts the generic enum to an <see cref="short"/>.  An exception will be thrown if the <see cref="Enum"/>
+        /// does not have a matching SizeOf.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static short AsShort<TEnum>(this TEnum enumValue) where TEnum : Enum
+        {
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 2)
+            {
+                throw new Exception("Type mismatch.");
+            }
+
+            return Unsafe.As<TEnum, short>(ref enumValue);
+        }
+
+        /// <summary>
+        /// Casts the generic enum to an <see cref="ushort"/>.  An exception will be thrown if the <see cref="Enum"/>
+        /// does not have a matching SizeOf.
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enumValue"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ushort AsUShort<TEnum>(this TEnum enumValue) where TEnum : Enum
+        {
+            // Make sure the TEnum is the proper size (hard coded short length for perf).
+            if (Unsafe.SizeOf<TEnum>() != 2)
+            {
+                throw new Exception("Type mismatch.");
+            }
+
+            return Unsafe.As<TEnum, ushort>(ref enumValue);
         }
 
         /// <summary>
