809b95120a9884a70c3736538f657965adf8e794
Blake Pell <blakepell@hotmail.com>
2022-07-03 12:07:12 -0400

    Consolidated files.

--- src/Argus.Core/IO/ReverseFileReader.cs
+++ src/Argus.Core/IO/ReverseFileReader.cs
@@ -10,6 +10,27 @@
 namespace Argus.IO
 {
     /// <summary>
+    /// Line endings for the ReverseFileReader supports.
+    /// </summary>
+    public enum LineEnding
+    {
+        /// <summary>
+        /// Carriage Return (character 13) and Line Feed (character 10)
+        /// </summary>
+        CrLf,
+
+        /// <summary>
+        /// Line feed (character 10)
+        /// </summary>
+        Lf,
+
+        /// <summary>
+        /// Carriage return (character 13)
+        /// </summary>
+        Cr
+    }
+
+    /// <summary>
     /// Reads lines from a file or a Stream in reverse order one line at a time.
     /// </summary>
     public class ReverseFileReader : IDisposable
