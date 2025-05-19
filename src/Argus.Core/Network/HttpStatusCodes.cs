43d5ccfd11744b7cf2ca87dc834a72f1738ee5ae
Blake Pell <blakepell@hotmail.com>
2025-05-19 11:45:45 -0400

    Refactors HTTP status code descriptions.
    Replaces the switch statement with a switch expression for retrieving HTTP status code descriptions, resulting in more concise and readable code. Also, updates the copyright to include 2025.

--- src/Argus.Core/Network/HttpStatusCodes.cs
+++ src/Argus.Core/Network/HttpStatusCodes.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * @author            : Blake Pell
  * @initial date      : 2018-12-12
- * @last updated      : 2018-12-12
- * @copyright         : Copyright (c) 2003-2024, All rights reserved.
+ * @last updated      : 2025-05-18
+ * @copyright         : Copyright (c) 2003-2025, All rights reserved.
  * @license           : MIT 
  * @website           : http://www.blakepell.com
  */
@@ -27,139 +27,74 @@ namespace Argus.Network
         /// <param name="statusCode"></param>
         public static string StatusDescription(int statusCode)
         {
-            switch (statusCode)
+            return statusCode switch
             {
-                case 100:
-                    return "Continue";
-                case 101:
-                    return "Switching Protocols";
-                case 102:
-                    return "Processing";
-                case 103:
-                    return "Early Hints";
-                case 200:
-                    return "OK";
-                case 201:
-                    return "Created";
-                case 202:
-                    return "Accepted";
-                case 203:
-                    return "Non-Authoritative Information";
-                case 204:
-                    return "No Content";
-                case 205:
-                    return "Reset Content";
-                case 206:
-                    return "Partial Content";
-                case 207:
-                    return "Multi-Status";
-                case 208:
-                    return "Already Reported";
-                case 226:
-                    return "IM Used";
-                case 300:
-                    return "Multiple Choices";
-                case 301:
-                    return "Moved Permanently";
-                case 302:
-                    return "Found / Moved Temporarily";
-                case 303:
-                    return "See Other";
-                case 304:
-                    return "Not Modified";
-                case 305:
-                    return "Use Proxy";
-                case 306:
-                    return "Switch Proxy";
-                case 307:
-                    return "Temporary Redirect";
-                case 308:
-                    return "Permanent Redirect";
-                case 400:
-                    return "Bad Request";
-                case 401:
-                    return "Unauthorized";
-                case 402:
-                    return "Payment Required";
-                case 403:
-                    return "Forbidden";
-                case 404:
-                    return "Not Found";
-                case 405:
-                    return "Method Not Allowed";
-                case 406:
-                    return "Not Acceptable";
-                case 407:
-                    return "Proxy Authentication Required";
-                case 408:
-                    return "Request Timeout";
-                case 409:
-                    return "Conflict";
-                case 410:
-                    return "Gone";
-                case 411:
-                    return "Length Required";
-                case 412:
-                    return "Precondition Failed";
-                case 413:
-                    return "Payload Too Large";
-                case 414:
-                    return "URI Too Long";
-                case 415:
-                    return "Unsupported Media Type";
-                case 416:
-                    return "Range Not Satisfiable";
-                case 417:
-                    return "Expectation Failed";
-                case 418:
-                    return "I'm a teapot";
-                case 421:
-                    return "Misdirected Request";
-                case 422:
-                    return "Unprocessable Entity";
-                case 423:
-                    return "Locked";
-                case 424:
-                    return "Failed Dependency";
-                case 426:
-                    return "Upgrade Required";
-                case 428:
-                    return "Precondition Required";
-                case 429:
-                    return "Too Many Requests";
-                case 431:
-                    return "Request Header Fields Too Large";
-                case 440:
-                    return "Login Time-out";
-                case 449:
-                    return "Retry With";
-                case 451:
-                    return "Unavailable for Legal Reasons";
-                case 500:
-                    return "Internal Server Error";
-                case 501:
-                    return "Not Implemented";
-                case 502:
-                    return "Bad Gateway";
-                case 503:
-                    return "Service Unavailable";
-                case 504:
-                    return "Gateway Timeout";
-                case 505:
-                    return "HTTP Version Not Supported";
-                case 506:
-                    return "Variant Also Negotiates";
-                case 507:
-                    return "Insufficient Storage";
-                case 508:
-                    return "Loop Detected";
-                case 510:
-                    return "Not Extended";
-                case 511:
-                    return "Network Authentication Required";
-                default:
-                    return "Unknown";
-            }
+                100 => "Continue",
+                101 => "Switching Protocols",
+                102 => "Processing",
+                103 => "Early Hints",
+                200 => "OK",
+                201 => "Created",
+                202 => "Accepted",
+                203 => "Non-Authoritative Information",
+                204 => "No Content",
+                205 => "Reset Content",
+                206 => "Partial Content",
+                207 => "Multi-Status",
+                208 => "Already Reported",
+                226 => "IM Used",
+                300 => "Multiple Choices",
+                301 => "Moved Permanently",
+                302 => "Found / Moved Temporarily",
+                303 => "See Other",
+                304 => "Not Modified",
+                305 => "Use Proxy",
+                306 => "Switch Proxy",
+                307 => "Temporary Redirect",
+                308 => "Permanent Redirect",
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                402 => "Payment Required",
+                403 => "Forbidden",
+                404 => "Not Found",
+                405 => "Method Not Allowed",
+                406 => "Not Acceptable",
+                407 => "Proxy Authentication Required",
+                408 => "Request Timeout",
+                409 => "Conflict",
+                410 => "Gone",
+                411 => "Length Required",
+                412 => "Precondition Failed",
+                413 => "Payload Too Large",
+                414 => "URI Too Long",
+                415 => "Unsupported Media Type",
+                416 => "Range Not Satisfiable",
+                417 => "Expectation Failed",
+                418 => "I'm a teapot",
+                421 => "Misdirected Request",
+                422 => "Unprocessable Entity",
+                423 => "Locked",
+                424 => "Failed Dependency",
+                426 => "Upgrade Required",
+                428 => "Precondition Required",
+                429 => "Too Many Requests",
+                431 => "Request Header Fields Too Large",
+                440 => "Login Time-out",
+                449 => "Retry With",
+                451 => "Unavailable for Legal Reasons",
+                500 => "Internal Server Error",
+                501 => "Not Implemented",
+                502 => "Bad Gateway",
+                503 => "Service Unavailable",
+                504 => "Gateway Timeout",
+                505 => "HTTP Version Not Supported",
+                506 => "Variant Also Negotiates",
+                507 => "Insufficient Storage",
+                508 => "Loop Detected",
+                510 => "Not Extended",
+                511 => "Network Authentication Required",
+                _ => "Unknown"
+            };
         }
     }
 }
